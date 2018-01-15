d2fe667c4acca58dfb7241181de37f651f701738
zerog2k <af5mi@yahoo.com>
2018-01-15 12:52:38 -0600

    cleanup 1

--- Output/blinky_pca10028_s130 debug/Obj/Cortex_M_Startup.d
+++
@@ -1,2 +0,0 @@
-Output/blinky_pca10028_s130\ debug/Obj/Cortex_M_Startup.o: \
- /Applications/SEGGER\ Embedded\ Studio\ 3.24/samples/Cortex_M_Startup.s
--- Output/blinky_pca10028_s130 debug/Obj/thumb_crt0.d
+++
@@ -1,2 +0,0 @@
-Output/blinky_pca10028_s130\ debug/Obj/thumb_crt0.o: \
- /Applications/SEGGER\ Embedded\ Studio\ 3.24/source/thumb_crt0.s
